97f013ea28af5b6b116fed296a9b1130b4bb981c
Karol <kar.mal@icloud.com>
2019-12-29 12:19:14 +0000

    setting the dice for a random number and adding pictures, also setting scores to zero

--- starter/app.js
+++ starter/app.js
@@ -15,4 +15,31 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = 6;
+//style helps us change css - here we dont want to see the dice
+document.querySelector(".dice").style.display = "none";
+
+//setting all the scores and currents to zero
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1 . Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2 Display the result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+  // 3 Update round score if the rolled number is not 1
+});
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+/*
+same as above but instead of text we use HTML
+query selector lets You chose items like css
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+*/
+
+// var x = document.querySelector("#score-0").textContent;
