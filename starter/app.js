bfea2484403180aecf37ef2aa0d14d8d7944607f
Karol <kar.mal@icloud.com>
2019-12-29 13:41:41 +0000

    added event if rolled 1, also toggled class active on both players

--- starter/app.js
+++ starter/app.js
@@ -33,6 +33,27 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   diceDOM.style.display = "block";
   diceDOM.src = "dice-" + dice + ".png";
   // 3 Update round score if the rolled number is not 1
+  if (dice !== 1) {
+    //Add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    //Next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    // toggle removes class when its there and adds when its not there
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // document.querySelector(".player-0-panel").classList.remove("active");
+    // document.querySelector(".player-1-panel").classList.add("active");
+
+    document.querySelector(".dice").style.display = "none";
+  }
 });
 
 // document.querySelector("#current-" + activePlayer).textContent = dice;
